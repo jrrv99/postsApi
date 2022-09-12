bb7d4c79551666b0f0ed1b8f50bea92beaf0af07
José Ricardo <jrrv99@gmail.com>
2022-09-12 03:10:27 -0400

    Styles: related to the basic styles config, varaibles, colors and resets

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import '@styles/reset.css';
+import '@styles/variables.css';
+import '@styles/colors.css';
 import '@styles/index.css';
 
 const container = document.getElementById('root');
